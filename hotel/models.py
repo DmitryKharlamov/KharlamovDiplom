a49b128f9301c6d86fb81005fe4e1dcbb1141ef5
Harlamov Dmitry <dmitry.kharlamov9706@gmail.com>
2024-10-13 19:04:45 +0500

    2 day

--- hotel/models.py
+++ hotel/models.py
@@ -7,12 +7,12 @@ from user.models import User
 # Create your models here.
 
 class Room(models.Model):
-    ROOM_CATEGORIES = (
-        ("ECO", "Эконом"),
-        ("SND", "Стандарт"),
-        ("PRE", "Премиум"),
-        ("LUX", "Люкс"),
-        ("KIN", "Президентский"),
+    ROOM_CATEGORIES=(
+    ("PRE", "Премиум"),
+    ("LUX", "Люкс"),
+    ("ECO","Эконом"),
+    ("STND","Стандарт"),
+    ("KING","Президентский"),
     )
     name = models.CharField(max_length=300, blank=True)
     number = models.IntegerField()
@@ -21,7 +21,7 @@ class Room(models.Model):
     adress = models.CharField(max_length=500, blank=True)
     Description = models.TextField(blank=True)
     facilities = models.TextField(blank=True)
-    category = models.CharField(max_length=3, choices=ROOM_CATEGORIES)
+    category = models.CharField(max_length=4, choices=ROOM_CATEGORIES)
     beds = models.IntegerField(default=1)
     capacity = models.IntegerField(default=1)
 
